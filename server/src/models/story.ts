500ea34f080b6c9135f112f46b5d132bf81a1261
Jeremy Osner <anacreon@gmail.com>
2025-02-26 09:36:16 -0500

    models

---
+++ server/src/models/story.ts
@@ -0,0 +1,21 @@
+import { Schema, Document } from 'mongoose';
+
+export interface IStory extends Document {
+	title: string;
+	content: string;
+	image_url: string;
+	category: string;
+	article_url: string;
+}
+
+export interface IFavoriteStory extends IStory {
+	_id: string;
+}
+
+export const Schema favoriteStorySchema<IFavoriteStory>({
+	title: String;
+	content: String;
+	image_url: String;
+	category: String;
+	article_url: String;
+});
