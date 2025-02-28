9170fb7b873f91e43577f9b86a06ab473c074af2
Joshua Clennon <clennonjoshua@gmail.com>
2025-02-28 10:50:29 -0500

    Saved Articles Page

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -4,7 +4,7 @@ import Home from "../pages/Home";
 import NewsDetail from "../pages/NewsDetail";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminPanel from "../pages/AdminPanel";
+import AdminPanel from "../pages/SavedArticles";
 
 export function AppRoutes() {
   const { user } = useAuth();
