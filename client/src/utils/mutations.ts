53ecbe4fb29d08b5aa094d7a7284cc8277fc2e56
Silv3rHRT <somethingcuteandgay@gmail.com>
2025-03-04 14:27:02 -0500

    fixed login and viteconfig to graphql

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,14 +1,16 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN = gql`mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+export const LOGIN = gql`mutation login($emailOrUsername: String!, $password: String!) {
+  login(emailOrUsername: $emailOrUsername, password: $password) {
+    token
+    user {
+      _id
+      email
+      username
+      password
     }
-  }`;
+  }
+}`;
 
 export const SIGN_UP = gql`mutation AddUser($username: String!, $email: String!, $password: String!) {
   addUser(username: $username, email: $email, password: $password) {
