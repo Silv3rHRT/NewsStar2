5a7f3a682e01c30d88f805c0c63eed10f28ef896
Silv3rHRT <somethingcuteandgay@gmail.com>
2025-03-04 11:24:57 -0500

    added queries and mutations

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -9,3 +9,47 @@ export const LOGIN = gql`mutation Login($email: String!, $password: String!) {
       }
     }
   }`;
+
+export const SIGN_UP = gql`mutation AddUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      email
+      username
+    }
+  }
+}`
+
+export const SEARCH = gql`mutation Mutation($searchTerms: String!, $from: Date, $to: Date, $sortBy: String) {
+  search(searchTerms: $searchTerms, from: $from, to: $to, sortBy: $sortBy) {
+    articleUrl
+    category
+    content
+    imageUrl
+    title
+  }
+}`
+
+export const ADD_FAVORITE= gql`mutation Mutation($title: String!, $content: String!, $imageUrl: String!, $category: String!, $articleUrl: String!) {
+  addFavorite(title: $title, content: $content, imageUrl: $imageUrl, category: $category, articleUrl: $articleUrl) {
+    _id
+    username
+    email
+    searchHistory {
+      _id
+      searchTerms
+      from
+      to
+      sortBy
+    }
+    favoriteStories {
+      _id
+      title
+      content
+      imageUrl
+      category
+      articleUrl
+    }
+  }
+}`
