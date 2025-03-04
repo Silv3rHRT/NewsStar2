35d97b8144cb1ae567160d162de6199a493dd9a1
Silv3rHRT <somethingcuteandgay@gmail.com>
2025-03-04 17:43:26 -0500

    ensure unique searches

--- server/src/models/searchParams.ts
+++ server/src/models/searchParams.ts
@@ -1,17 +1,36 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model, Document } from "mongoose";
 
 export interface ISearchParams extends Document {
-	searchTerms: string;
-	to: Date;
-	from: Date;
-	sortBy: string;
+  searchTerms: string;
+  to: Date;
+  from: Date;
+  sortBy: string;
 }
 
 export const searchParamsSchema = new Schema<ISearchParams>({
-	searchTerms: { type: String, required: true },
-	to: { type: Date, required: false },
-	from: { type: Date, required: false },
-	sortBy: { type: String, required: false },
+  searchTerms: {
+	type: String,
+	required: true,
+	unique: true
+},
+  to: {
+	type: Date,
+	required: false,
+	unique:true 
+},
+  from: {
+	type: Date,
+	required: false,
+	unique: true
+ },
+  sortBy: {
+	type: String,
+	required: false,
+	unique: true
+},
 });
 
-export const SearchParams = model<ISearchParams>('SearchParams', searchParamsSchema)
+export const SearchParams = model<ISearchParams>(
+  "SearchParams",
+  searchParamsSchema
+);
